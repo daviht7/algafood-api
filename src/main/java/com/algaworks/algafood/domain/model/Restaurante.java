807331f277d0604c923f5e1f8b3a01808a1d8c79
Davi Holanda <daviht7@gmail.com>
2023-04-15 20:19:09 -0300

    3.17 - Mapeando relacionamento com @ManyToOne

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -5,11 +5,7 @@ import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -25,5 +21,8 @@ public class Restaurante {
 	
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
+
+	@ManyToOne()
+	private Cozinha cozinha;
 
 }
