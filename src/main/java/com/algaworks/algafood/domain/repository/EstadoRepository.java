02044fa9ec15c07e8468e83ac5b518a248d9a817
Davi Holanda <daviht7@gmail.com>
2023-04-17 22:55:13 -0300

    4.11 - Desafio collection resource de estados

--- src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/EstadoRepository.java
@@ -1,4 +1,11 @@
 package com.algaworks.algafood.domain.repository;
 
+import com.algaworks.algafood.domain.model.Estado;
+
+import java.util.List;
+
 public interface EstadoRepository {
+
+    List<Estado> listar();
+
 }
