dc8f5205dc0f760e43bae558ff6ee67855924299
Davi Holanda <daviht7@gmail.com>
2023-04-18 21:41:26 -0300

    4.13 - Implementando content negotiation para retornar JSON ou XML

--- src/main/java/com/algaworks/algafood/api/controllers/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controllers/EstadoController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controllers;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class EstadoController {
     @Autowired
     private EstadoRepository estadoRepository;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Estado> listar() {
         return estadoRepository.listar();
     }
