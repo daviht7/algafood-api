e7ade23a49586583cc0fcceaf45c9b4f8df2dd3e
Davi Holanda <daviht7@gmail.com>
2023-06-04 12:28:31 -0300

    5.8 - Conhecendo os prefixos de query methods

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,9 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+
+    Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+
+    List<Restaurante> findTop2RestauranteByNomeContaining(String nome);
+
+    boolean existsByNome(String nome);
+
+    int countByCozinhaId(Long cozinha);
 
 }
